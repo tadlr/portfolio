721905e19017874fa910db9b5841f014f86354df
Gustavo Ramirez <gustavoramirezr@icloud.com>
2025-01-08 21:41:04 -0500

    Add Node.js CI workflow, NVMRC file with version, and update Next.js config
    - Added a Node.js CI workflow for building and testing node projects
    - Included an NVMRC file specifying the Node.js version
    - Updated Next.js config to set output, reactStrictMode, trailingSlash, devIndicators, and eslint configurations.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  reactStrictMode: false,
+  trailingSlash: true,
+  devIndicators: {
+    buildActivity: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
 };
 
 export default nextConfig;
